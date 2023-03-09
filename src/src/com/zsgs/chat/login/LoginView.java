275234155a81ed125489f77f871fb6b1473e0b40
Sowndaryasankar <115859421+Sowndaryasankar@users.noreply.github.com>
2023-03-09 16:41:14 +0530

    Add files via upload

---
+++ src/src/com/zsgs/chat/login/LoginView.java
@@ -0,0 +1,55 @@
+package com.zsgs.chat.login;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import com.zsgs.chat.dao.User;
+import com.zsgs.chat.home.HomeView;
+
+public class LoginView {
+	private LoginController loginController;
+	private Scanner scanner = new Scanner(System.in);
+
+	public LoginView() {
+		loginController = new LoginController(this);
+	}
+	
+	static public void main(String[] args) {
+		LoginView loginView = new LoginView();
+		loginView.create();
+	}
+	
+	public void create() {
+		checkForLogin();
+	}
+
+	private void checkForLogin() {
+		System.out.println("Welcome to chat application\n ------------------------");
+		try {
+		System.out.println("Enter User Name");
+		String userName = scanner.next();
+		System.out.println("Enter password");
+		String password = scanner.next();
+		loginController.checkCredentials(userName, password);	
+		}catch(InputMismatchException e)
+		{
+			checkForLogin();
+		}
+		
+		
+	}
+
+	public void loginSuccess(User user) {
+		System.out.println("\n-->Hello " + user.getUserName() + "<--\n");
+		HomeView homeView = new HomeView();
+		homeView.create(user);
+	}
+
+	public void loginFailed(String errorMessage) {
+		System.out.println(errorMessage);
+		checkForLogin();
+	}
+
+}
+
+
